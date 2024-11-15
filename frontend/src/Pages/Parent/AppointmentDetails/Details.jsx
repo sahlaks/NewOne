2f27cf08c40c734f2259ac6c8609a5159a56ec3c
sahla <sahlaanasks@gmail.com>
2024-11-15 20:46:19 +0530

    'feedback'

--- frontend/src/Pages/Parent/AppointmentDetails/Details.jsx
+++ frontend/src/Pages/Parent/AppointmentDetails/Details.jsx
@@ -4,17 +4,21 @@ import "../../Doctor/Consultation/Details.css";
 import ParentHeader from "../../../Components/Header/ParentHeader";
 import Loading from "../../../Components/Loading/Loading";
 import Footer from "../../../Components/Footer/Footer";
-import { fetchPrescription } from "../../../utils/parentFunctions";
+import {
+  fetchPrescription,
+  submitReview,
+} from "../../../utils/parentFunctions";
 import jsPDF from "jspdf";
+import { toast } from "react-toastify";
+import ParentFeedbackModal from "../../../Components/Review/ReviewAndRating";
 
 const ParentAppointmentDetails = () => {
   const [loading, setLoading] = useState(false);
   const [prescriptions, setPrescriptions] = useState([]);
   const { state } = useLocation();
   const { appointment } = state || {};
+  const [isParentModalOpen, setIsParentModalOpen] = useState(false);
 
-  console.log('details', appointment);
-  
   const getStatusColor = (status) => {
     switch (status) {
       case "Completed":
@@ -45,6 +49,25 @@ const ParentAppointmentDetails = () => {
     fetchPrescriptionData();
   }, [appointment]);
 
+  const handleReview = async (feedback) => {
+    const trimmedReview = feedback.review.trim();
+    if (!trimmedReview) {
+      toast.error("Feedback cannot be empty. Please provide your review.");
+      return;
+    }
+
+    const res = await submitReview(
+      feedback,
+      appointment?.doctorId,
+      appointment?.parentName
+    );
+    if (res.success) toast.success(res.message);
+  };
+
+  const handleSubmission = () => {
+    setIsParentModalOpen(true);
+  };
+
   const downloadPrescription = () => {
     const doc = new jsPDF();
     let lineY = 20;
@@ -54,7 +77,7 @@ const ParentAppointmentDetails = () => {
     doc.text("CalmNest Medical Prescription", 20, lineY);
     lineY += 10;
     doc.setDrawColor(100, 149, 237);
-    doc.line(20, lineY += 10, 190, lineY);
+    doc.line(20, (lineY += 10), 190, lineY);
 
     lineY += 10;
     doc.setFontSize(14);
@@ -90,20 +113,24 @@ const ParentAppointmentDetails = () => {
     doc.setTextColor(0, 0, 0);
     doc.setFontSize(12);
     prescriptions.forEach((rec, index) => {
-      doc.text(`${index + 1}. ${rec.text}`, 20, lineY += 10);
+      doc.text(`${index + 1}. ${rec.text}`, 20, (lineY += 10));
     });
 
     doc.setDrawColor(100, 149, 237);
-    doc.line(20, lineY += 15, 190, lineY);
+    doc.line(20, (lineY += 15), 190, lineY);
 
     lineY += 10;
     doc.setFontSize(10);
     doc.setTextColor(0, 0, 0);
     doc.text("Contact Information", 20, lineY);
     doc.setTextColor(105, 105, 105);
-    doc.text("Email: info@calmnest.com", 20, lineY += 6);
-    doc.text("Phone: +1 234 567 890", 20, lineY += 6);
-    doc.text("Address: 123 Calm Street, Trithala, Palakkad, Kerala 679534", 20, lineY += 6);
+    doc.text("Email: info@calmnest.com", 20, (lineY += 6));
+    doc.text("Phone: +1 234 567 890", 20, (lineY += 6));
+    doc.text(
+      "Address: 123 Calm Street, Trithala, Palakkad, Kerala 679534",
+      20,
+      (lineY += 6)
+    );
     doc.save("Prescription.pdf");
   };
 
@@ -114,34 +141,58 @@ const ParentAppointmentDetails = () => {
         <Loading />
       ) : (
         <div className="bg-[#FAF5E9] min-h-screen flex flex-col items-center pt-16 px-4">
-          <h1 className="text-3xl font-semibold text-center mb-8">Appointment Details</h1>
+          <h1 className="text-3xl font-semibold text-center mb-8">
+            Appointment Details
+          </h1>
 
           <div className="w-full md:w-3/4 lg:w-2/3 bg-[#E3D7CD] rounded-lg shadow-md p-6 mb-10">
-            <h2 className="text-2xl font-semibold text-[#323232] mb-4">Appointment Overview</h2>
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-2xl font-semibold text-[#323232]">
+                Appointment Overview
+              </h2>
+              <button
+                className="px-4 py-2 bg-[#323232] text-white font-semibold rounded shadow hover:bg-black transition-all"
+                onClick={handleSubmission}
+              >
+                Review Doctor
+              </button>
+            </div>
             <div className="flex justify-between mb-4">
-              <p>Date: {appointment?.date}  </p>
+              <p>Date: {appointment?.date} </p>
               <p>Start Time: {appointment?.startTime}</p>
-              <p className={`${getStatusColor(appointment?.appointmentStatus)} font-semibold`}>
+              <p
+                className={`${getStatusColor(appointment?.appointmentStatus)} font-semibold`}
+              >
                 Status: {appointment?.appointmentStatus}
               </p>
             </div>
-            <h2 className="text-xl font-semibold text-[#323232] mt-6">Patient Information</h2>
+            <h2 className="text-xl font-semibold text-[#323232] mt-6">
+              Patient Information
+            </h2>
             <p>Parent Name: {appointment?.parentName}</p>
             <p>Child Name: {appointment?.name}</p>
             <p>Age: {appointment?.age}</p>
             <p>Gender: {appointment?.gender}</p>
-            <h2 className="text-xl font-semibold text-[#323232] mt-6">Doctor Information</h2>
+            <h2 className="text-xl font-semibold text-[#323232] mt-6">
+              Doctor Information
+            </h2>
             <p>Doctor Name: {appointment?.doctorName}</p>
-            <h2 className="text-xl font-semibold text-[#323232] mt-6">Appointment Information</h2>
+            <h2 className="text-xl font-semibold text-[#323232] mt-6">
+              Appointment Information
+            </h2>
             <p>Fee: {appointment?.fees}</p>
             <p>Payment Status: {appointment?.paymentStatus}</p>
           </div>
 
           {appointment?.prescription && prescriptions.length > 0 ? (
             <div className="w-full md:w-3/4 lg:w-2/3 bg-[#E3D7CD] rounded-lg shadow-md p-6 mb-10">
-              <h2 className="text-xl font-semibold text-[#323232] mb-4">Prescription</h2>
+              <h2 className="text-xl font-semibold text-[#323232] mb-4">
+                Prescription
+              </h2>
               {prescriptions.map((rec, index) => (
-                <p key={rec.id}>{index + 1}. {rec.text}</p>
+                <p key={rec.id}>
+                  {index + 1}. {rec.text}
+                </p>
               ))}
               <button
                 onClick={downloadPrescription}
@@ -151,10 +202,17 @@ const ParentAppointmentDetails = () => {
               </button>
             </div>
           ) : (
-            <p className="text-center text-gray-500">No prescription available</p>
+            <p className="text-center text-gray-500">
+              No prescription available
+            </p>
           )}
         </div>
       )}
+      <ParentFeedbackModal
+                isOpen={isParentModalOpen}
+                onClose={() => setIsParentModalOpen(false)}
+                onSubmit={handleReview}
+            />
       <Footer />
     </>
   );
