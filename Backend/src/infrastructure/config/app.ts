712b844be021108afc39a1e91a3c391d051afef2
sahla <sahlaanasks@gmail.com>
2024-11-22 00:34:41 +0530

    'files'

--- Backend/src/infrastructure/config/app.ts
+++ Backend/src/infrastructure/config/app.ts
@@ -39,7 +39,7 @@ const createServer = () => {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use('/uploads', express.static(path.resolve(__dirname, '../../uploads')));
-    console.log(path.resolve(__dirname, '../../../uploads'));
+    console.log(path.resolve(__dirname, '../../uploads'));
     
     app.use('/api/parents',parentRouter)
     app.use('/api/doctor', doctorRouter)
